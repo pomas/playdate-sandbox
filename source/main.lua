4a459fa0f4088e4d890c56562149dbedb1f5462f
Jan Pomahac <j.pomahac@psp.team>
2023-03-25 15:21:30 +0100

    kralicek bezi doleva

--- source/main.lua
+++ source/main.lua
@@ -7,10 +7,13 @@ local gfx <const> = playdate.graphics
 local playerSprite = nil
 
 
+local kralicekMoves = 1
 local kralicekLeft = gfx.image.new("Images/kralicekright")
 local kralicekRight = gfx.image.new("Images/kralicekleft")
+local kralicekRight2 = gfx.image.new("Images/kralicekright2")
 assert( kralicekLeft )
 assert( kralicekRight )
+assert( kralicekRight2 )
 
 
 
@@ -45,7 +48,13 @@ function playdate.update()
         
     end
     if playdate.buttonIsPressed( playdate.kButtonRight ) then
-        playerSprite:setImage(kralicekRight)
+        if(kralicekMoves == 1) then 
+            playerSprite:setImage(kralicekRight)
+            kralicekMoves = 0
+        else 
+            playerSprite:setImage(kralicekRight2)
+            kralicekMoves = 1
+        end
         playerSprite:moveBy( 2, 0 )
     end
     if playdate.buttonIsPressed( playdate.kButtonDown ) then
